69ccc0996421e496df0bb4bcc155dfce157e0b77
Kishan1735 <gswwarriorsnba@gmail.com>
2023-08-23 19:24:30 +0530

    fixes

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -4,7 +4,7 @@ function Skills() {
   return (
     <div className="container w-screen min-w-screen my-0 pt-0 p-2 md:p-10 md:pt-0 h-full min-h-screen 2xl:h-screen  text-black  bg-slate-900 text-center md:text-start">
       <NavBar />
-      <div className="relative p-10 rounded-xl my-5 bg-white opacity-90 shadow-lg">
+      <div className="relative p-10 rounded-xl my-5 bg-white opacity-90 shadow-lg min-w-screen">
         <div className="flex z-10 space-x-4 justify-center md:justify-start">
           <div className="text-3xl">
             <h2 className="max-w-lg mt-10 md:mt-15 mb-6 font-sans text-6xl font-bold text-center text-gray-900 uppercase md:text-5xl md:mt-0 md:text-left">
